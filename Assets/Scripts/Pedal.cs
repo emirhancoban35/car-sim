19b45178192198da01bec66fc29d0cc370eac287
Emirhan Çoban <117065693+emirhancoban35@users.noreply.github.com>
2025-01-29 14:28:49 +0300

    add car class

---
+++ Assets/Scripts/Pedal.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pedal
+{
+   public Pedals pedalName;
+   public bool isPressed;
+}
---
+++ Assets/Scripts/Pedals.cs
@@ -0,0 +1,3 @@
+public enum Pedals{
+    accelerator,brake,clutch
+}   
