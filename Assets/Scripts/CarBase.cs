0ee1680fda7321a644639de172314a37b5c7841f
Ximitie <162482939+Ximitie@users.noreply.github.com>
2025-02-07 18:04:56 +0300

    Model Changes

--- Assets/Scripts/CarBase.cs
+++ Assets/Scripts/CarBase.cs
@@ -7,13 +7,6 @@ public class CarBase : MonoBehaviour
     private CarPedal _carPedal;
     private CarTransmissionManager _carTransmissionManager;
     private CarDial _carDial;
-    private CarSteering _carSteering;
-
-    [Header("Wheel Colliders")]
-    [SerializeField] private WheelCollider[] wheelColliders;
-
-    [Header("Steering Wheel")]
-    [SerializeField] private Transform steeringWheel; 
 
     private void Awake()
     {
@@ -21,28 +14,8 @@ public class CarBase : MonoBehaviour
         _carPedal = new CarPedal(carData);
         _carTransmissionManager = new CarTransmissionManager(carData);
         _carDial = new CarDial(carData);
-        _carSteering = new CarSteering(carData, wheelColliders, steeringWheel);
 
         _carTransmissionManager.GenerateTransmission();
-    }
-    private void UpdateWheelTransforms()
-    {
-        foreach (var wheelCollider in wheelColliders)
-        {
-            wheelCollider.GetWorldPose(out var position, out var rotation);
-            wheelCollider.transform.position = position;
-            wheelCollider.transform.rotation = rotation;
-        }
-    }
-    
-    private void FixedUpdate()
-    {
-        if (!carData.isMotorRunning) return;
-
-        float steeringInput = CarInputHandler.GetSteeringInput();
-        _carSteering.UpdateSteering(steeringInput);
-
-        UpdateWheelTransforms(); // Tekerlek modellerini güncelle
     }
 
     private void Update()
@@ -51,8 +24,6 @@ public class CarBase : MonoBehaviour
         _carPedal.HandlePedals();
         _carTransmissionManager.HandleTransmission();
         _carDial.UpdateDial();
-
-        float steeringInput = CarInputHandler.GetSteeringInput();
-        _carSteering.UpdateSteering(steeringInput);
+        
     }
 }
