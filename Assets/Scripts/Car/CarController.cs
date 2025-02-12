ed94cb64ab2e23e48f29bc9f5e8d81370b916bd5
Emirhan Çoban <117065693+emirhancoban35@users.noreply.github.com>
2025-02-12 16:39:35 +0300

    optimize dial codes

--- Assets/Scripts/Car/CarController.cs
+++ Assets/Scripts/Car/CarController.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 public class CarController : MonoBehaviour
@@ -22,7 +21,7 @@ public class CarController : MonoBehaviour
         _carTransmissionManager = new CarTransmissionManager(carData);
         _carSteering = new CarSteering(carData, steeringWheel, frontWheels);
         _carEngineController = new CarEngineController(carData);
-        _carMovementController = new CarMovementController(carData , frontWheels);
+        _carMovementController = new CarMovementController(carData, frontWheels);
 
         _carTransmissionManager.GenerateTransmission();
     }
@@ -34,52 +33,8 @@ public class CarController : MonoBehaviour
         _carTransmissionManager.UpdateTransmission();
         _carEngineController.UpdateEngine();
         _carMovementController.UpdateMovement();
-        
-        
 
         float steeringInput = CarInputHandler.GetSteeringInput();
         _carSteering.UpdateSteering(steeringInput);
     }
-
-    //Esgal Kodları Başladı
-    //using System; ben ekledim
-    [SerializeField] private Rigidbody _rigidbody;
-    public CarDial carDial = new CarDial();
-
-    public event Action<float> OnRpmChanged;
-    public event Action<float> OnSpeedChanged;
-    public event Action<float> OnTemperatureChanged;
-    public event Action<float> OnFuelChanged;
-
-    private void FixedUpdate()
-    {
-        UpdateCarStats();
-        NotifyGaugeUpdates();
-    }
-
-    private void UpdateCarStats()
-    {
-        float speedKmh = _rigidbody.velocity.magnitude * 3.6f; // m/s -> km/h dönüşümü
-        carDial.currentSpeed = Mathf.Lerp(carDial.currentSpeed, speedKmh, Time.deltaTime * 5f);
-
-        // Devir sayacı (RPM) hesaplama
-        float maxRPM = 8000f;
-        carDial.tachometer = Mathf.Lerp(carDial.tachometer, Mathf.Clamp(speedKmh * 30, 0, maxRPM), Time.deltaTime * 3f);
-
-        // Motor sıcaklığı yavaş yavaş artar
-        carDial.temperatureIndicator = Mathf.Lerp(carDial.temperatureIndicator, 90f, Time.deltaTime * 0.1f);
-
-        // Yakıt seviyesi azalmaya devam eder
-        carDial.fuelLevel = Mathf.Max(0, carDial.fuelLevel - Time.deltaTime * 0.001f);
-    }
-
-    private void NotifyGaugeUpdates()
-    {
-        OnRpmChanged?.Invoke(carDial.tachometer);
-        OnSpeedChanged?.Invoke(carDial.currentSpeed);
-        OnTemperatureChanged?.Invoke(carDial.temperatureIndicator);
-        OnFuelChanged?.Invoke(carDial.fuelLevel);
-    }
-
-    //Esgal Kodları Bitti
 }
